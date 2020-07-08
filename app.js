0df4d6b0caa7a6b72ddb2066663fa2fa158220c9
ns <znxpsxk8@naver.com>
2020-07-08 11:31:08 +0900

    project

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var ejs = require('ejs')
+var app = express()
+require('dotenv').config()
+var apiRouter  = require('./routes/router')
+
+app.set('views', path.resolve(__dirname + '/views'))
+app.set('view engine' , 'ejs')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+var cors = require('cors')
+app.use(cors())
+
+app.get('/' ,(req, res, next) =>{
+    res.send('Success')
+})
+
+app.use('/', apiRouter)
+
+var port = process.env.PORT || 3000
+app.listen(port , ()=>{
+    console.log(`Server is Starting at http://localhost:${port}`)
+})
